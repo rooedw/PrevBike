8abc80d16e23742b6d4db4a02de20d4ea225dcf7
Edwin <96492204+rooedw@users.noreply.github.com>
2025-02-26 16:16:02 +0100

    Initial commit

---
+++ src/main/java/de/example/prevbike/PrevBikeApplication.java
@@ -0,0 +1,13 @@
+package de.example.prevbike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrevBikeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrevBikeApplication.class, args);
+    }
+
+}
