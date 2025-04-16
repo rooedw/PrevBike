a0d1a7e215fbfd3fcf11a4922b7e7373e557698f
Jonas Muth <Jonas.Muth@outlook.com>
2025-04-16 19:58:21 +0200

    Button not blue anymore

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,11 @@
 import { useState } from 'react'
 import prevBikeLogo from './assets/prevbike_logo_landscape.png'
-import './App.css'
 import React from 'react';
 import { Container, Row, Col, Button, Form, InputGroup, Collapse } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'leaflet/dist/leaflet.css';
 import { MapContainer, TileLayer, Marker, Popup, ZoomControl } from 'react-leaflet';
-
+import './App.css'
 
 function App() {
     const position = [49.0087159, 8.403911516];
@@ -16,7 +15,7 @@ function App() {
         <Container fluid className="vh-100 d-flex flex-column">
             <Row className="flex-grow-1">
                 {/* Sidebar */}
-                <Col md={3} className="p-3" style={{ backgroundColor: '#FFDCB9', color: 'black' }}>
+                <Col md={2} className="p-3" style={{ backgroundColor: '#FFDCB9', color: 'black' }}>
                     <Form>
                         <Form.Group className="mb-3">
                             <Form.Label>Stadt</Form.Label>
@@ -36,46 +35,50 @@ function App() {
                             <Button variant="secondary" className="mt-2">Suchpositionsmarker platzieren</Button>
                         </Form.Group>
 
-                        <Form.Group className="mb-3">
+                        <Form.Group className="mb-3 p-2" style={{border: '2px solid darkgray'}}>
                             <Form.Label
                                 onClick={() => setShowAdvanced(!showAdvanced)}
                                 aria-controls="advanced-options"
                                 aria-expanded={showAdvanced}
-                                style={{ cursor: 'pointer', display: 'block' }}
+                                style={{ cursor: 'pointer', display: 'block', fontWeight: 'bold' }}
                             >
                                 Erweiterte Optionen {showAdvanced ? '▲' : '▼'}
                             </Form.Label>
                             <Collapse in={showAdvanced}>
                                 <div id="advanced-options">
-                                    <InputGroup className="mb-2">
+                                    <Form.Label>Position</Form.Label>
+                                    <InputGroup className="mb-2 ">
                                         <InputGroup.Text>Lat</InputGroup.Text>
-                                        <Form.Control type="text" defaultValue="49.123456" />
+                                        <Form.Control type="text" defaultValue="49.123456"/>
                                     </InputGroup>
-                                    <InputGroup className="mb-3">
+
+                                    <InputGroup className="mb-3 ">
                                         <InputGroup.Text>Lon</InputGroup.Text>
-                                        <Form.Control type="text" defaultValue="8.123456" />
+                                        <Form.Control type="text" defaultValue="8.123456"/>
                                     </InputGroup>
 
                                     <Form.Label>Betrachtungszeitraum</Form.Label>
-                                    <InputGroup className="mb-3">
-                                        <Form.Control type="number" defaultValue={2} />
+                                    <InputGroup className="mb-3 ">
+                                        <Form.Control type="number" defaultValue={2}/>
                                         <InputGroup.Text>Wochen</InputGroup.Text>
                                     </InputGroup>
 
-                                    <Form.Label>Zeitrahmen (+/-)</Form.Label>
-                                    <InputGroup className="mb-3">
-                                        <Form.Control type="number" defaultValue={30} />
-                                        <InputGroup.Text>Min</InputGroup.Text>
-                                    </InputGroup>
+                                        <Form.Label>Zeitrahmen (+/-)</Form.Label>
+                                        <InputGroup className="mb-3 ">
+                                            <Form.Control type="number" defaultValue={30}
+                                                          className="short-input-smaller"/>
+                                            <InputGroup.Text>Min</InputGroup.Text>
+                                        </InputGroup>
 
-                                    <Form.Check type="checkbox" label="Modus: selber Wochentag" defaultChecked />
-                                </div>
+                                        <Form.Check type="checkbox" label="Modus: selber Wochentag" defaultChecked/>
+                                    </div>
                             </Collapse>
                         </Form.Group>
 
 
-                        <Button variant="light" className="mb-2">Suche speichern</Button>
-                        <Button variant="dark" className="mb-3">Suche starten</Button>
+                        <Button variant="light" className="mb-3">Suche speichern</Button>
+                        <Button variant="light" className="mb-3">Suche laden</Button>
+                        <Button variant="primary" className="mb-3">Suche starten</Button>
 
                         <div className="bg-light text-dark p-2">
                             Wahrscheinlichkeit für Zeitraum x - y: <strong>10%</strong>
@@ -87,7 +90,7 @@ function App() {
                 </Col>
 
                 {/* Main View */}
-                <Col md={9} className="p-0 position-relative">
+                <Col md={10} className="p-0 position-relative">
                     <div className="rounded-header d-flex justify-content-between align-items-center px-4">
                         <Button variant="light" size="sm">🌐 DE</Button>
 
