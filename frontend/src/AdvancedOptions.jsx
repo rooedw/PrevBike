a4e643cc60f5faf5b293d649209a8367e0a8aaf1
Jonas Muth <Jonas.Muth@outlook.com>
2025-05-02 16:43:10 +0200

    Put AdvancedOptions in new component

---
+++ frontend/src/AdvancedOptions.jsx
@@ -0,0 +1,50 @@
+import React, {useState} from 'react';
+import {Collapse, Form, InputGroup} from "react-bootstrap";
+
+function AdvancedOptions () {
+    const [showAdvanced, setShowAdvanced] = useState(false);
+
+    return (
+        <Form.Group className="mb-3 p-2" style={{border: '2px solid darkgray'}}>
+            <Form.Label
+                onClick={() => setShowAdvanced(!showAdvanced)}
+                aria-controls="advanced-options"
+                aria-expanded={showAdvanced}
+                style={{ cursor: 'pointer', display: 'block', fontWeight: 'bold' }}
+            >
+                Erweiterte Optionen {showAdvanced ? '▲' : '▼'}
+            </Form.Label>
+            <Collapse in={showAdvanced}>
+                <div id="advanced-options">
+                    <Form.Label>Position</Form.Label>
+                    <InputGroup className="mb-2 ">
+                        <InputGroup.Text>Lat</InputGroup.Text>
+                        <Form.Control type="text" defaultValue="49.123456"/>
+                    </InputGroup>
+
+                    <InputGroup className="mb-3 ">
+                        <InputGroup.Text>Lon</InputGroup.Text>
+                        <Form.Control type="text" defaultValue="8.123456"/>
+                    </InputGroup>
+
+                    <Form.Label>Betrachtungszeitraum</Form.Label>
+                    <InputGroup className="mb-3 ">
+                        <Form.Control type="number" defaultValue={2}/>
+                        <InputGroup.Text>Wochen</InputGroup.Text>
+                    </InputGroup>
+
+                    <Form.Label>Zeitrahmen (+/-)</Form.Label>
+                    <InputGroup className="mb-3 ">
+                        <Form.Control type="number" defaultValue={30}
+                                      className="short-input-smaller"/>
+                        <InputGroup.Text>Min</InputGroup.Text>
+                    </InputGroup>
+
+                    <Form.Check type="checkbox" label="Modus: selber Wochentag" defaultChecked/>
+                </div>
+            </Collapse>
+        </Form.Group>
+    );
+}
+
+export default AdvancedOptions;
